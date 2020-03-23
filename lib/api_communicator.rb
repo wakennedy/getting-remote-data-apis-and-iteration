82a4602eee3cc7591c01937b323c14761d2a15d0
wakennedy <alexkenned@gmail.com>
2020-03-23 10:17:03 -0700

    even with the bonus

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,32 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
+def get_character_movie_urls_from_api(character_name)
+  films = []
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  found = response_hash["results"].select do |result| 
+      result["name"] == character_name
+  end
+  found = found[0]["films"]
+end
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+def parse_urls(string)
+  string.map do |url|
+    bad_strings = RestClient.get(url)
+    JSON.parse(bad_strings)
+  end
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  titles = films.map do |films|
+    films["title"]
+  end
+  puts titles
 end
 
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
+  urls = get_character_movie_urls_from_api(character)
+  strings = parse_urls(urls)
+  print_movies(strings)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Hello, welcome to my mess of code!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+
+  gets.chomp
 end
